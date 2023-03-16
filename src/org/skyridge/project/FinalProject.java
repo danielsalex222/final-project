390fd33dfba4c377122a58ff875b14b478e2bb9e
danielsalex222 <127963331+danielsalex222@users.noreply.github.com>
2023-03-15 22:10:29 -0600

    Update FinalProject.java

--- src/org/skyridge/project/FinalProject.java
+++ src/org/skyridge/project/FinalProject.java
@@ -3,5 +3,7 @@ package org.skyridge.project;
 public class FinalProject {
     public static void main(String[] args) {
         System.out.println("Yeah!!! Hello world");
+        System.out.println("More Lines");
+        System.out.println("Hello Again");
     }
 }
